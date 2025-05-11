860929974f8989b7a7523b6eb431418f974accb9
maddox-blay <156577870+maddox-blay@users.noreply.github.com>
2025-05-11 13:15:00 +0000

    added basic calculator behavior to html

---
+++ index.js
@@ -0,0 +1,84 @@
+const nums = document.querySelectorAll(".number");
+const disp = document.querySelector("#bigOp");
+const clear = document.querySelector("#clear");
+const prev = document.querySelector("#prevOp");
+const equals = document.querySelector("#equals");
+const percent = document.querySelector("#percent");
+const operations = document.querySelectorAll(".operation"); 
+let complete = false;
+const svg = `<svg width="28" height="23" viewBox="0 0 28 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M19.4395 5.93945C20.0253 5.35373 20.9749 5.35369 21.5606 5.93945C22.1463 6.52522 22.1463 7.47478 21.5606 8.06055L18.6212 11L21.5606 13.9395L21.6642 14.0537C22.1442 14.6428 22.1096 15.5115 21.5606 16.0605C21.0116 16.6095 20.143 16.6441 19.5538 16.1641L19.4395 16.0605L16.5001 13.1211L13.5606 16.0605C12.9749 16.6463 12.0253 16.6463 11.4395 16.0605C10.8538 15.4748 10.8538 14.5252 11.4395 13.9395L14.379 11L11.4395 8.06055L11.336 7.94629C10.8559 7.35714 10.8905 6.48848 11.4395 5.93945C11.9886 5.39049 12.8573 5.35584 13.4464 5.83594L13.5606 5.93945L16.5001 8.87891L19.4395 5.93945Z" fill="#FDF7F7"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M25.0001 0C26.3807 6.87173e-05 27.5001 1.11933 27.5001 2.5V20C27.5001 21.3807 26.3807 22.4999 25.0001 22.5H11.4298C10.8302 22.5 10.2528 22.2851 9.80185 21.8984L9.61728 21.7217L1.08408 12.7402C0.150305 11.7573 0.17034 10.2087 1.129 9.25L9.64658 0.732422L9.83017 0.566406C10.275 0.201994 10.8341 3.50001e-05 11.4142 0H25.0001ZM11.9142 2C11.5662 2.00003 11.2308 2.12126 10.964 2.33984L10.8536 2.43945L3.33408 9.95898C2.76011 10.533 2.7465 11.4597 3.3038 12.0498L10.8409 20.0303L10.9522 20.1367C11.2231 20.3699 11.57 20.4999 11.9308 20.5H24.0001C24.8285 20.4999 25.5001 19.8284 25.5001 19V3.5C25.5001 2.67161 24.8285 2.00006 24.0001 2H11.9142Z" fill="#FDF7F7"/>
+</svg>
+
+`
+
+operations.forEach(operation => {
+    operation.addEventListener("click", event => {
+    if (disp.textContent.slice(-1) != event.target.textContent && event.target.textContent != "=") {
+        disp.textContent += event.target.textContent;
+    }    
+})
+})
+percent.addEventListener("click", event => {
+    if (!(disp.textContent.slice(-1) == event.target.textContent)) {
+        disp.textContent += event.target.textContent;
+    }    
+})
+
+
+equals.addEventListener("click", event =>{
+    prev.textContent = disp.textContent
+    clear.innerHTML = "AC"
+    complete = true;
+})
+
+clear.addEventListener("click", event =>{
+    if (disp.textContent == "0" || disp.textContent.length == 1 || (disp.textContent.length == 2 && disp.textContent.includes("0."))) {
+        clear.innerHTML = "AC"
+    }
+    if (clear.innerHTML == "AC") {
+        disp.textContent = "0";
+        prev.textContent = ""; 
+    } else {
+        disp.textContent = disp.textContent.slice(0, -1);
+    }
+    console.log(clear.innerHTML == "AC")
+    
+})
+
+nums.forEach(element => {
+ element.addEventListener("click",event => {
+    if (event.target.textContent != 0) {
+        clear.innerHTML = svg;
+    }
+    if (disp.textContent == "0" && event.target.textContent != ".") {
+        disp.textContent = event.target.textContent;
+    }else if(!(disp.textContent.includes(".") && event.target.textContent == ".")){
+        disp.textContent += event.target.textContent;
+    }
+    if (complete) {
+        disp.textContent = event.target.textContent;
+        complete = false;
+    }
+    prev.textContent = "";
+})   
+});
+
+
+const add = function(num1, num2){
+    return num1 + num2;
+}
+
+const subtract = function(num1, num2){
+    return num1 - num2;
+}
+
+const divide = function(num1, num2){
+    return num1 / num2;
+}
+
+const multiply = function(num1, num2){
+    return num1 * num2;
+}
+
